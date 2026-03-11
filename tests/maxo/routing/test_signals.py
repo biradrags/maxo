3ce162aed5325fce456ff0c5bc215566c020b69b
K1rL3s <kirill.lesovoi@mail.ru>
2026-03-11 18:37:39 +0300

    Тесты на установку состояний обсерверам и мидлварям по сигналам

--- tests/maxo/routing/test_signals.py
+++ tests/maxo/routing/test_signals.py
@@ -5,6 +5,11 @@ import pytest
 from maxo import Router
 from maxo.enums import ChatType
 from maxo.routing.dispatcher import Dispatcher
+from maxo.routing.middlewares.state import (
+    EmptyMiddlewareManagerState,
+    StartedMiddlewareManagerState,
+)
+from maxo.routing.observers.state import EmptyObserverState, StartedObserverState
 from maxo.routing.signals import (
     AfterShutdown,
     AfterStartup,
@@ -114,6 +119,41 @@ async def test_included_router_signals() -> None:
         *(["before_shutdown"] * 2),
         *(["after_shutdown"] * 2),
     ]
+
+
+@pytest.mark.asyncio
+async def test_included_router_observers_state() -> None:
+    # ruff: noqa: E721
+    dp = Dispatcher()
+    deep_router = Router()
+    deeper_router = Router()
+
+    dp.include(deep_router)
+    deep_router.include(deeper_router)
+
+    for router in (dp, deep_router, deeper_router):
+        for observer in router.observers.values():
+            assert type(observer.state) == EmptyObserverState
+            assert type(observer.middleware.inner.state) == EmptyMiddlewareManagerState
+
+    await dp.feed_signal(BeforeStartup())
+
+    for router in (dp, deep_router, deeper_router):
+        for observer in router.observers.values():
+            assert type(observer.state) == StartedObserverState
+            assert (
+                type(observer.middleware.inner.state) == StartedMiddlewareManagerState
+            )
+
+    await dp.feed_signal(AfterStartup())
+    await dp.feed_signal(BeforeShutdown())
+
+    for router in (dp, deep_router, deeper_router):
+        for observer in router.observers.values():
+            assert type(observer.state) == EmptyObserverState
+            assert type(observer.middleware.inner.state) == EmptyMiddlewareManagerState
+
+    await dp.feed_signal(AfterShutdown())
 
 
 @pytest.mark.asyncio
